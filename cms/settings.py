c70df5fffe5fc1fa17e353a712d5962bf3f25098
刘康 <LLiuXZ@yeah.net>
2019-02-27 16:43:34 +0800

    发送手机注册短信验证码

--- cms/settings.py
+++ cms/settings.py
@@ -148,6 +148,13 @@ CACHES = {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
     },
+    "verify_codes": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
 }
 # 保存 session数据到 Redis中，主要给django的admin后台使用
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
